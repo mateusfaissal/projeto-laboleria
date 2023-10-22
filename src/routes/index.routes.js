d9f2c3d2ffccf57f7bd499ea8de47216e6a80def
Mateus Faissal <mateusfaissal@gmail.com>
2023-10-21 22:50:48 -0300

    third commit: project structure created

---
+++ src/routes/index.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import cakesRouter from "./cakes.routes.js"
+import clientsRouter from "./clients.router.js"
+import ordersRouter from "./orders.routes.js"
+
+const router = Router();
+
+router.use(cakesRouter);
+router.use(clientsRouter)
+router.use(ordersRouter);
+
+export default router;
